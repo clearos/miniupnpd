8ecfefe6f45109ca96442863b7706ccdff068087
Nick Howitt <nickjh@github.com>
2018-03-13 08:17:24 +0000

    Klugde for when WAN is down - force WAN to lo

--- init_clearos.sh
+++ init_clearos.sh
@@ -13,6 +13,11 @@
 # Determine WAN interface default route
 MINIUPNPD_WAN="-i `LC_ALL=C ip -4 route | grep 'default' | sed -e 's/.*dev[[:space:]]*//' -e 's/[[:space:]].*//'`"
 
+# kludge for when WAN goes down to stop miniupnpd failing
+if [ "$MINIUPNPD_WAN" = "-i " ]; then
+	MINIUPNPD_WAN="-i lo"
+fi
+
 # List all the LAN's with the correct config parameter
 MINIUPNPD_LANS=""
 for LAN in $LANIF; do
@@ -26,7 +31,7 @@ fi
 # Should really be done as part of the installation. Done here for safety.
 [ -f /var/lib/miniupnpd/upnp.leases ] || touch /var/lib/miniupnpd/upnp.leases
 
-systemctl set-environment MINIUPNPD_OPTIONS="$MINIUPNPD_WAN $MINIUPNPD_LANS $MINIUPNPD_OPTIONS"
+systemctl set-environment MINIUPNPD_START_OPTIONS="$MINIUPNPD_WAN $MINIUPNPD_LANS $MINIUPNPD_OPTIONS"
 
 exit 0
 
